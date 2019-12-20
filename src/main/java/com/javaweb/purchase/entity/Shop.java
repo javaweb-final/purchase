2917e482a697aef6f72a735209f902579c08bd39
Weiss Schnee <zxh000109@163.com>
2019-12-20 11:50:44 +0800

    add shop

---
+++ src/main/java/com/javaweb/purchase/entity/Shop.java
@@ -0,0 +1,47 @@
+package com.javaweb.purchase.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@TableName(value = "boss")
+@EqualsAndHashCode(callSuper = false)
+public class Shop {
+    private Integer id;
+    private String shopname;
+    private String username;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getShopname() {
+        return shopname;
+    }
+
+    public void setShopname(String shopname) {
+        this.shopname = shopname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
