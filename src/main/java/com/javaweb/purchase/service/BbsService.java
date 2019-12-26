179a384d2ef0149a4535c904f551f9e32318c372
unknown <792110498@qq.com>
2019-12-26 19:08:20 +0800

    数据处理方面的部分修改

--- src/main/java/com/javaweb/purchase/service/BbsService.java
+++ src/main/java/com/javaweb/purchase/service/BbsService.java
@@ -7,14 +7,13 @@ import com.javaweb.purchase.mapper.BbsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
+import java.util.List;
 @Service
 public class BbsService extends ServiceImpl<BbsMapper, Bbs> {
     @Autowired
     private BbsMapper bbsMapper;
     public List<BbsUserVo> findAllBbs(){
-
         return bbsMapper.findAllBbs();
     }
     public List<BbsUserVo> findInfo(Integer id){
